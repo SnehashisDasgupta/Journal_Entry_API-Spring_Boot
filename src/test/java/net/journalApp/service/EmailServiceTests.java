a27cdd33be7ca8a904f2ed2dde3e83d3b1424acc
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-09-29 12:10:00 +0530

    Sending email through spring boot

---
+++ src/test/java/net/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,19 @@
+package net.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail() {
+        emailService.sendEmail("dustbin732@gmail.com",
+                "Test email 1",
+                "Testing chal rhi hai");
+    }
+}
